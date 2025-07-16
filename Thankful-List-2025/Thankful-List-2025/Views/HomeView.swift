cbe59009de4dc2efc08534f223fa16318b5d2e48
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-07-16 09:07:32 -0400

    add Thanks image to assets, add Views folder to project, move all views to Views folder.

---
+++ Thankful-List-2025/Thankful-List-2025/Views/HomeView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeView.swift
+//  Thankful-List-2025
+//
+//  Created by Thomas Cowern on 7/16/25.
+//
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            VStack(spacing: 30) {
+                
+                Image("Thanks")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200)
+                
+                Text("Welcome to the Grateful List!")
+                    .font(.title)
+                    .padding()
+                
+                Text("The Grateful List exists to help you keep track of people, places, experiences and things that you are grateful for in your life.")
+                    .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}

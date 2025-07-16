de573025a746816ff34e9e2f8be5dd1d8a6fe03d
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-07-16 08:47:01 -0400

    add Thankful-List-2025 base project to folder

---
+++ Thankful-List-2025/Thankful-List-2025/Thankful_List_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Thankful_List_2025App.swift
+//  Thankful-List-2025
+//
+//  Created by Thomas Cowern on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Thankful_List_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

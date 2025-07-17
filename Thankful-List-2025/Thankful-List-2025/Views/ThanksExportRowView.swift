7a55c58552ffa1f7dc0e2dffb5f6d01f783a068c
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-07-17 09:30:01 -0400

    add ThanksExportRowView and ThanksExportView to experiment with pdf export

---
+++ Thankful-List-2025/Thankful-List-2025/Views/ThanksExportRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ThanksExportRowView.swift
+//  Thankful-List-2025
+//
+//  Created by Thomas Cowern on 7/17/25.
+//
+
+import SwiftUI
+
+struct ThanksExportRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThanksExportRowView()
+}

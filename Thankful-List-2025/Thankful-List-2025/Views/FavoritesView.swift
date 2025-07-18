a2355e32c3ecb46c8cef2dcdaf345a9e60d6b7ba
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-07-18 08:47:27 -0400

    update FavoritesView with new toolbar

--- Thankful-List-2025/Thankful-List-2025/Views/FavoritesView.swift
+++ Thankful-List-2025/Thankful-List-2025/Views/FavoritesView.swift
@@ -6,8 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct FavoritesView: View {
+    
+    @Environment(\.modelContext) private var modelContext
+    @State private var path = NavigationPath()
+    @Query var thanks: [Thanks]
+    @State private var searchText = ""
+    @State private var sortOrder = [SortDescriptor(\Thanks.title)]
+    
+    init(sort: SortDescriptor<Thanks>) {
+        _thanks = Query(sort: [sort])
+    }
+    
     var body: some View {
         NavigationStack {
             Text("Favorites View")
@@ -15,9 +27,36 @@ struct FavoritesView: View {
                     print("Add tapped")
                 }
         }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Name A->Z")
+                            .tag([SortDescriptor(\Thanks.title)])
+                        
+                        Text("Name Z->A")
+                            .tag([SortDescriptor(\Thanks.title, order: .reverse)])
+                        
+                        Text("Date 1->30")
+                            .tag([SortDescriptor(\Thanks.date)])
+                        
+                        Text("Date 30->1")
+                            .tag([SortDescriptor(\Thanks.date, order: .reverse)])
+                    }
+                }
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Image(systemName: "plus")
+                    .onTapGesture {
+                        let newThanks = Thanks(title: "", body: "", date: Date.now, isFavorite: false, icon: IconImages.star.rawValue, color: "#007AFF")
+                        modelContext.insert(newThanks)
+                        path.append(newThanks)
+                    }
+            }
+        }
     }
 }
 
 #Preview {
-    FavoritesView()
+    FavoritesView(sort: SortDescriptor(\Thanks.title))
 }

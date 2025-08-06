84ea4ae9f53efc9abc7d748dc19392ce7ce4e881
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-08-06 09:23:54 -0400

    update SettingsTip with image

--- Thankful-List-2025/Thankful-List-2025/Tips/SettingsTip.swift
+++ Thankful-List-2025/Thankful-List-2025/Tips/SettingsTip.swift
@@ -10,5 +10,6 @@ import TipKit
 
 public struct SettingsTip: Tip {
     public var title: Text { Text("Change Settings") }
-    public var message: Text { Text("You can set reminders and delete your Thanks here." )}
+    public var message: Text? { Text("You can set reminders and delete your Thanks here." )}
+    public var image: Image? { Image(systemName: "arrowshape.down.fill") }
 }

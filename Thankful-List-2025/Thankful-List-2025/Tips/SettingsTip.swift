c5f84ff721a55543bf227db8d613bfa77b28bdd0
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-08-06 08:48:52 -0400

    add SettingsTip to project in Tips folder

---
+++ Thankful-List-2025/Thankful-List-2025/Tips/SettingsTip.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsTip.swift
+//  Thankful-List-2025
+//
+//  Created by Thomas Cowern on 8/6/25.
+//
+
+import Foundation
+import TipKit
+
+public struct SettingsTip: Tip {
+    public var title: Text { Text("Change Settings") }
+    public var message: Text { Text("You can set reminders and delete your Thanks here." )}
+}
